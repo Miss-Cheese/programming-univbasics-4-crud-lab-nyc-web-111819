89d3dffd9903234ad2caabc64b7a476b14545c6c
Natalie Galligan <nat.galligan@gmail.com>
2019-11-04 01:06:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   marathon_friends = ["Lyubava","Masha","Cooke","Maria"] 
-  marathon_friends.push("Natasha")
+  marathon_friends.push("arrays!")
   
 end
 
